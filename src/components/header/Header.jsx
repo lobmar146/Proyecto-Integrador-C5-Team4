b38852bc99b38c5f7bee16b973dd80b4f6244268
lobmar146 <lobmar146@gmail.com>
2023-08-01 18:08:10 -0300

    [Feat] Menu Hamburguesa - Styled Components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,36 +1,130 @@
-import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { BsFillBrightnessHighFill } from 'react-icons/bs'
+import styled from 'styled-components'
+import { useState } from 'react'
 import { GiSoccerBall } from 'react-icons/gi'
-import { FaTooth } from 'react-icons/fa6'
+import BurguerButton from './BurgerButton'
 
 export default function Header() {
-  const [open, setOpen] = useState(false)
+  const [clicked, setClicked] = useState(false)
 
   const handleClick = () => {
-    setOpen(!open)
+    setClicked(!clicked)
   }
-
   return (
-    <header className='sticky'>
+    <HeaderWrapper>
       {/* Contenedor logo del header */}
-      <div className='contenedor-flex-header'>
-        {/* logo */}
-        <Link to='/'>
-          <div>
-            <h1>
-              <GiSoccerBall />
-              4Football
-            </h1>
-          </div>
-        </Link>
-      </div>
+
+      {/* logo */}
+      <Link to='/'>
+        <h1>
+          <GiSoccerBall />
+          4Football
+        </h1>
+      </Link>
 
       {/* Contenedor de los botones */}
-      <div className='contenedor-flex-header'>
-        <button>Iniciar Sesion</button>
-        <button>Registrarse</button>
+      <div className={`nav-container ${clicked ? 'active' : ''}`}>
+        <Link to='#'>Iniciar Sesion </Link>
+        <Link to='#'>Crear Usuario </Link>
       </div>
-    </header>
+
+      <div className='burger'>
+        <BurguerButton clicked={clicked} handleClick={handleClick} />
+      </div>
+      <BgDiv className={`initial ${clicked ? ' active' : ''}`}></BgDiv>
+    </HeaderWrapper>
   )
 }
+const HeaderWrapper = styled.header`
+  z-index: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 5rem;
+  width: 100%;
+  height: 5rem;
+
+  position: sticky;
+  margin-top: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: black;
+
+  h1 {
+    color: white;
+  }
+
+  .nav-container a {
+    color: white;
+    padding: 0.5rem;
+    text-decoration: none;
+    border: 0.1rem solid white;
+    border-radius: 5rem;
+    margin-right: 1rem;
+  }
+
+  .nav-container {
+    position: absolute;
+    top: -700px;
+    left: -2000px;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    a {
+      color: black;
+      font-size: 1.5rem;
+      display: block;
+    }
+    @media (min-width: 768px) {
+      position: initial;
+      margin: 0;
+      a {
+        display: inline;
+        color: white;
+        font-size: 1rem;
+      }
+    }
+  }
+
+  .nav-container.active {
+    width: 100%;
+    display: block;
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    top: 317%;
+    left: 0;
+    right: 0;
+    text-align: center;
+    a {
+      color: white;
+      border-radius: 0;
+      border: none;
+      border-bottom: 1px solid white;
+    }
+  }
+  .burger {
+    @media (min-width: 768px) {
+      display: none;
+    }
+  }
+`
+const BgDiv = styled.div`
+  background-color: black;
+  position: absolute;
+  top: -1000px;
+  left: -1000px;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  transition: all 0.6s ease;
+
+  &.active {
+    border-radius: 0 0 80% 0;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+`
