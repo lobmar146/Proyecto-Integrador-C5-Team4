1bc1a5421be881c53e5973c98780f1e71ffd5920
= <=>
2023-08-03 19:08:48 -0300

    [FIX] Año dinámico en el footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,28 +1,36 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
 
-import styled from 'styled-components'
-
-import { GiSoccerBall } from 'react-icons/gi'
-
+import { GiSoccerBall } from "react-icons/gi";
 
 export default function Footer() {
+  const [year, setYear] = useState(0);
 
-  return(
+  const updateYear = () => {
+    let date = new Date();
+    let fullYear = date.getFullYear();
+    setYear(fullYear);
+  };
+
+  useEffect(() => {
+    updateYear();
+  }, []);
+
+  return (
     <>
-    <StyleFooter>
-          
-         <div>  <GiSoccerBall /> 4Football 2023  &copy; </div>
-          
-           </StyleFooter>
+      <StyleFooter>
+        <div>
+          {" "}
+          <GiSoccerBall /> 4Football {year} &copy;{" "}
+        </div>
+      </StyleFooter>
     </>
-  )
-
+  );
 }
 const StyleFooter = styled.footer`
-align-items: center;
-background-color: grey; 
-display: flex;
-height: 2rem;
-justify-content: center;
-
+  align-items: center;
+  background-color: grey;
+  display: flex;
+  height: 2rem;
+  justify-content: center;
 `;
-
