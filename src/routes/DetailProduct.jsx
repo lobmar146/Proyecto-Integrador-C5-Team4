8f43cfb96a2eb146a1f772284dfead9f2df2ca4b
= <=>
2023-08-05 19:36:48 -0300

    [REFACTOR] Nuevos estilos para el DetailProduct

--- src/routes/DetailProduct.jsx
+++ src/routes/DetailProduct.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { BsFillArrowRightSquareFill } from 'react-icons/bs'
 
 export const DetailProduct = () => {
   return (
-    <section className="content">
+    <DetailProductWrapper className="content">
       <h1>Botines</h1>
+      <img
+        src="https://newsport.vtexassets.com/arquivos/ids/13500130/FW9212-A.jpg?v=637883033205600000"
+        alt="botines nike"
+      />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio debitis
         enim consequuntur, quam quisquam deleniti magni suscipit vero recusandae
@@ -24,7 +30,29 @@ export const DetailProduct = () => {
         consectetur ipsa totam veniam reprehenderit, nobis reiciendis voluptate
         sit sapiente eos atque, cumque fugit possimus non tempora aliquam saepe.
       </p>
-      <Link to={"/"}>Volver</Link>
-    </section>
+      <Link to={"/"} className="link">
+        <BsFillArrowRightSquareFill />
+      </Link>
+    </DetailProductWrapper>
   );
 };
+
+const DetailProductWrapper = styled.section`
+  padding: 4rem;
+  line-height: 1.2rem;
+
+  img {
+    width: 200px;
+  }
+
+  .link {
+    float: right;
+    font-size: 40px;
+    color: #302d2d;
+    transition: color 0.6s;
+  }
+
+  .link:hover {
+    color: #000000;
+  }
+`
